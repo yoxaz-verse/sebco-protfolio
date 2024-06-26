c73b900ef0ba586003e89220177b8e6fb3c8fc46
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-06-26 13:02:37 +0530

    map componnet ssr removal

--- src/components/ProjectDetail/images-row.tsx
+++ src/components/ProjectDetail/images-row.tsx
@@ -7,7 +7,7 @@ const ImagesRow = ({ data }: any) => {
       <div className='flex flex-row  overflow-x-scroll gap-2 p-2 justify-between'>
         {data.map((image: any, index: any) => (
           <div key={index} className='w-full'>
-            <Image src={image} width={1000} height={400} alt={index} className='h-[220px]' />
+            <Image src={image} width={400} height={400} alt={image.status} className='h-[220px]' />
           </div>
         ))}
       </div>
