a50d88b67ceebc97a10d84fa40d9a3510578e79a
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-01-11 17:29:54 +0530

    Team

--- src/components/OurStaff/our-staff-hero.tsx
+++ src/components/OurStaff/our-staff-hero.tsx
@@ -1,14 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
-import Animate from '../ReUseComponents/Animate'
+import Image from "next/image";
+import React from "react";
+import Animate from "../ReUseComponents/Animate";
 
 const OurStaffPageHero = () => {
   return (
     <Animate>
-    <Image src={'/ourstaff/ourstaff.png'} width={1920} height={800} alt='Our Staff Hero Image'/>
-    
+      <Image
+        src={"/ourapproaches/ourapproach4.png"}
+        width={1920}
+        height={800}
+        alt="Our Staff Hero Image"
+      />
     </Animate>
-  )
-}
+  );
+};
 
-export default OurStaffPageHero
+export default OurStaffPageHero;
--- src/components/OurStaff/team-card.tsx
+++ src/components/OurStaff/team-card.tsx
@@ -18,20 +18,36 @@ const OurTeamCard = ({ data, type }: OurTeamCardProps) => {
       {error ? (
         <OurTeamCardError onReload={handleReload} />
       ) : (
-        <Card className={`rounded-none ${type == 'home' && 'text-white border-none shadow-none'}`} style={{ backgroundColor: type == 'home' ? 'transparent' : 'white' }}>
+        <Card
+          className={`rounded-none max-w-[220px] ${
+            type == "home" && "text-white border-none shadow-none"
+          }`}
+          style={{ backgroundColor: type == "home" ? "transparent" : "white" }}
+        >
           <CardBody className="flex justify-center items-center p-0">
             <Image
               src={data.image}
               width={800}
               height={800}
+              className="w-[200px] h-max rounded-sm "
               alt="Our Team Member"
               onError={handleImageError}
             />
-            <div className={`flex flex-row gap-2 py-0 my-0 ${type === 'home' && 'text-start w-10/12 py-2'}`}>
-              <h1>{data.name},</h1>
-              <h1>{data.role}</h1>
+            <div
+              className={`flex flex-col items-center  py-0 my-0 ${
+                type === "home" && "text-start w-10/12 py-2"
+              }`}
+            >
+              <span className="font-bold">{data.name}</span>
+              <span className="text-xs">{data.role}</span>
             </div>
-            <div className={`my-0 py-0 ${type === 'home' && 'text-start text-sm pb-5 w-10/12 py-3'}`}>{type == 'home' ? data.description : data.designation}</div>
+            <div
+              className={`my-0 py-0 ${
+                type === "home" && "text-start text-sm pb-5 w-10/12 py-3"
+              }`}
+            >
+              {type == "home" ? data.description : data.designation}
+            </div>
           </CardBody>
         </Card>
       )}
