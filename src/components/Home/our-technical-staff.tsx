2bc8584526af90376988fbaa9594c05e52263160
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-01-11 17:54:47 +0530

    Staff

--- src/components/Home/our-technical-staff.tsx
+++ src/components/Home/our-technical-staff.tsx
@@ -8,34 +8,38 @@ import { useFetchData } from "@/hooks/useFetchData";
 import { Titles } from "@/data/admintitle";
 import { Spinner } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { Link } from "react-alice-carousel";
 
 const OurTechnicalStaffComponent = () => {
   const { data, loading } = useFetchData(Titles.Our_Staff);
   const navigation = useRouter();
   if (loading) {
-    return (
-      <Spinner color="warning" content="Loading.." />
-    )
+    return <Spinner color="warning" content="Loading.." />;
   }
   if (!loading) {
     const staff = Object.values(data) || [];
     return (
       <div className="w-full bg-[#373737] rounded-md flex flex-col p-4 items-center my-20">
         <div className="flex justify-between items-center w-full md:w-[95%] pt-6 relative">
-          <div className="text-[#FFBD12] text-2xl md:text-3xl text-start font-medium pb-6 md:pb-5 z-50 absolute w-full">
+          <Link
+            href={"/staff"}
+            className="text-[#FFBD12] text-2xl md:text-3xl text-start font-medium pb-6 md:pb-5 z-50 absolute w-full "
+          >
             Our Technical Staff
-          </div>
-          <div className="flex justify-end items-end z-10 pb-4 md:pb-5 w-full">
-          </div>
+          </Link>
+          <div className="flex justify-end items-end z-10 pb-4 md:pb-5 w-full"></div>
           <div className="flex justify-center items-center md:justify-end md:items-end z-10 pb-4 md:pb-20">
-            <div onClick={() => navigation.push("/staff")} className="bg-[#2C2B2B] h-6 w-6 md:h-10 md:w-10 rounded-full flex justify-center items-center cursor-pointer">
+            <Link
+              href={"/staff"}
+              className="bg-[#2C2B2B] h-6 w-6 md:h-10 md:w-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
               <FaArrowRight className="text-white text-sm md:text-lg" />
-            </div>
+            </Link>
           </div>
         </div>
-        <div className="flex flex-wrap gap-4 lg:gap-10 pb-5 flex-col md:flex-row">
-          {staff.splice(0, 3).map((staffdetails: any, index: any) => (
-            <Animate key={index} className="w-full md:w-[30%]">
+        <div className="flex flex-wrap justify-evenly w-full  gap-4  pb-5 ">
+          {staff.splice(0, 5).map((staffdetails: any, index: any) => (
+            <Animate key={index} className=" flex flex-wrap w-max  ">
               <OurTeamCard data={staffdetails} key={index} type="home" />
             </Animate>
           ))}
